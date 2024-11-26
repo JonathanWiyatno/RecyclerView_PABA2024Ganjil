405b94183b982fab8cd0180cd6d230d1f10a0f8c
Joy <c14210035@john.petra.ac.id>
2024-11-26 19:28:44 +0700

    Tambah Shared Preferences

--- app/src/main/java/uts/c14210035/recyclerview/MainActivity.kt
+++ app/src/main/java/uts/c14210035/recyclerview/MainActivity.kt
@@ -2,6 +2,7 @@ package uts.c14210035.recyclerview
 
 import android.content.DialogInterface
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.provider.ContactsContract.Data
 import android.widget.Toast
@@ -19,16 +20,20 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import uts.c14210035.recyclerview.ui.theme.RecyclerViewTheme
 
 class MainActivity : ComponentActivity() {
 
-    private lateinit var _nama : MutableList<String>
-    private lateinit var _karakter : MutableList<String>
-    private lateinit var _deskripsi : MutableList<String>
-    private lateinit var _gambar : MutableList<String>
+    private var _nama : MutableList<String> = emptyList<String>().toMutableList()
+    private var _karakter : MutableList<String> = emptyList<String>().toMutableList()
+    private var _deskripsi : MutableList<String> = emptyList<String>().toMutableList()
+    private var  _gambar : MutableList<String> = emptyList<String>().toMutableList()
 
     private var arWayang = arrayListOf<wayang>()
+
+    lateinit var sp : SharedPreferences
 
     private lateinit var _rvWayang : RecyclerView
 
@@ -38,10 +43,36 @@ class MainActivity : ComponentActivity() {
 
         setContentView(R.layout.activity_main)
 
+        sp = getSharedPreferences("dataSP", MODE_PRIVATE)
+
+        val gson =  Gson()
+        val isiSP = sp.getString("spWayang", null)
+        val type = object : TypeToken<ArrayList<wayang>> () {}.type
+
+        if (isiSP!=null) {
+            arWayang = gson.fromJson(isiSP, type)
+        }
+
         _rvWayang = findViewById<RecyclerView>(R.id.rvWayang)
-        SiapkanData()
+        if (arWayang.size == 0) {
+            SiapkanData()
+        }
+        else {
+            arWayang.forEach {
+                _nama.add(it.nama)
+                _gambar.add(it.foto)
+                _deskripsi.add(it.foto)
+                _deskripsi.add(it.deskripsi)
+                _karakter.add(it.karakter)
+            }
+
+            arWayang.clear()
+        }
+
         TambahData()
         TampilkanData()
+
+
 
     }
 
@@ -53,6 +84,8 @@ class MainActivity : ComponentActivity() {
     }
 
     fun TambahData(){
+        val gson = Gson()
+        val editor = sp.edit()
         arWayang.clear()
         for (position in _nama.indices){
             val data = wayang(
@@ -65,6 +98,10 @@ class MainActivity : ComponentActivity() {
             arWayang.add(data)
 
         }
+
+        val json = gson.toJson(arWayang)
+        editor.putString("spWayang", json)
+        editor.apply()
 
     }
 
